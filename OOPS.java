ea409480fa55df3941dfc54c67b966d665bac944
Atul <141128988+Atulsandilya5@users.noreply.github.com>
2025-03-05 10:25:26 +0530

    Add files via upload

---
+++ OOPS.java
@@ -0,0 +1,28 @@
+class Pen{
+    String color;
+    String type;
+
+    public void write(){
+        System.out.println("Writing something");
+    }
+    public void printColor(){
+        System.out.println(this.color);
+    }
+}
+
+
+public class OOPS {
+    public static void main(String[] args) {
+        Pen pen1=new Pen();
+        pen1.color="blue";
+        pen1.type="ballpoint";
+
+        Pen pen2=new Pen();
+        pen2.color="Black";
+        pen2.type="Gel";
+
+        pen1.printColor();
+        pen2.printColor();
+
+    }
+}
